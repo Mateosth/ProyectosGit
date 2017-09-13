9889b2e3b4d5b3c2f31d8534d85f0dc7ff50fa30
HenrySerna <mateosth@yahoo.es>
2017-09-13 16:35:45 -0500

    Trabajo Inicial

---
+++ app/src/main/java/lhst/lenguajeavan2/com/taller/MainActivity.java
@@ -0,0 +1,31 @@
+package lhst.lenguajeavan2.com.taller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+Button button;
+    TextView textView;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
+        textView=(TextView) findViewById(R.id.textView);
+        editText=(EditText) findViewById(R.id.editText);
+    }
+
+    public void clickboton(View V) {
+        textView.setText(editText.getText());
+        Toast.makeText(this,"Usted oprimió el botón",Toast.LENGTH_SHORT).show();
+    }
+}
